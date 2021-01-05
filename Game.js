d84045b5536c9e784783a672dbb8871ddce64d55
PVMS-Aasish <73524800+PVMS-Aasish@users.noreply.github.com>
2021-01-05 12:06:18 +0530

    Add files via upload

--- Game.js
+++ Game.js
@@ -44,11 +44,36 @@ char2.addImage("char2",char2Img);
 text.addImage("text",textImg);
 mhp.addAnimation("flying",mhp_flying);
 
+life.addImage("life",lifeImg);
+life1.addImage("life1",life1Img);
+life2.addImage("life2",life2Img);
+life3.addImage("life3",life3Img);
+life4.addImage("life4",life4Img);
+
+dementors1.addImage("dementors1",dementors1Img);
+dementors2.addImage("dementors2",dementors2Img);
+dementors3.addImage("dementors3",dementors3Img);
+dementors4.addImage("dementors4",dementors4Img);
+dementors5.addImage("dementors5",dementors5Img);
+dementors6.addImage("dementors6",dementors6Img);
+dementors7.addImage("dementors7",dementors7Img);
+dementors8.addImage("dementors8",dementors8Img);
+dementors9.addImage("dementors9",dementors9Img);
+dementors10.addImage("dementors10",dementors10Img);
+
+
+
+
 mhp.collide(invground);
 mhp.collide(inv2);
-//dementors.addImage("dementors",dementorsImg);
-
-
+if (frameCount % 150 === 0){
+  dementors=createSprite(950,700,2000,10);
+  dementors.addImage("dementors",dementorsImg);
+  dementors.velocityX = -8;
+  dementors.scale = 0.5;
+  dementors.y=Math.round(random(200,700));     
  
+  dementors.lifetime = 300;
+}
   }
 }
--- sketch.js
+++ sketch.js
@@ -15,6 +15,14 @@ var dementors,dementorsImg,dementorsGroup;
 var text,textImg;
 var mhp,mhp_flying;
 var inv2;
+var life,lifeImg;
+var life1,life1Img;
+var life2,life2Img;
+var life3,life3Img;
+var life4,life4Img;
+var dementors1,dementors2,dementors3,dementors4,dementors5,dementors6,dementors7,dementors8,dementors9,dementors10;
+var dementors1Img,dementors2Img,dementors3Img,dementors4Img,dementors5Img,dementors6Img,dementors7Img,dementors8Img,dementors9Img,dementors10Img;
+var dementorsLEFT = 0;
 
 function preload(){
 
@@ -26,8 +34,22 @@ function preload(){
     char2Img=loadImage("hermoine.png");
 textImg=loadImage("text.png");
 mhp_flying=loadAnimation("mhp1.png","mhp2.png","mhp3.png","mhp4.png","mhp5.png","mhp6.png","mhp7.png")
-   // dementorsImg-loadImage("harrypotter_dementor.png");
-   
+   dementorsImg=loadImage("harrypotter_dementor.png");
+   lifeImg=loadImage("life.png");
+   life1Img=loadImage("life.png");
+   life2Img=loadImage("life.png");
+   life3Img=loadImage("life.png");
+   life4Img=loadImage("life.png");
+   dementors1Img=loadImage("harrypotter_dementor.png");
+   dementors2Img=loadImage("harrypotter_dementor.png");
+   dementors3Img=loadImage("harrypotter_dementor.png");
+   dementors4Img=loadImage("harrypotter_dementor.png");
+   dementors5Img=loadImage("harrypotter_dementor.png");
+   dementors6Img=loadImage("harrypotter_dementor.png");
+   dementors7Img=loadImage("harrypotter_dementor.png");
+   dementors8Img=loadImage("harrypotter_dementor.png");
+   dementors9Img=loadImage("harrypotter_dementor.png");
+   dementors10Img=loadImage("harrypotter_dementor.png");
 }
 
 function setup(){
@@ -72,7 +94,56 @@ mhp.visible=false;
 mhp.scale=0.40;
 mhp.velocityY=1;
 //dementors =createSprite(700,800,1,1);
+life=createSprite(100,40,1,1);
+life.scale=0.060;
+life.visible=false;
 
+life1=createSprite(150,40,1,1);
+life1.scale=0.060;
+life1.visible=false;
+
+life2=createSprite(200,40,1,1);
+life2.scale=0.060;
+life2.visible=false;
+
+life3=createSprite(250,40,1,1);
+life3.scale=0.060;
+life3.visible=false;
+
+life4=createSprite(300,40,1,1);
+life4.scale=0.060;
+life4.visible=false;
+
+dementors1=createSprite(400,40,10,10);
+dementors1.scale=0.10;
+dementors1.visible=false;
+dementors2=createSprite(430,40,10,10);
+dementors2.scale=0.10;
+dementors2.visible=false;
+dementors3=createSprite(460,40,10,10);
+dementors3.scale=0.10;
+dementors3.visible=false;
+dementors4=createSprite(490,40,10,10);
+dementors4.scale=0.10;
+dementors4.visible=false;
+dementors5=createSprite(520,40,10,10);
+dementors5.scale=0.10;
+dementors5.visible=false;
+dementors6=createSprite(550,40,10,10);
+dementors6.scale=0.10;
+dementors6.visible=false;
+dementors7=createSprite(580,40,10,10);
+dementors7.scale=0.10;
+dementors7.visible=false;
+dementors8=createSprite(610,40,10,10);
+dementors8.scale=0.10;
+dementors8.visible=false;
+dementors9=createSprite(640,40,10,10);
+dementors9.scale=0.10;
+dementors9.visible=false;
+dementors10=createSprite(670,40,10,10);
+dementors10.scale=0.10;
+dementors10.visible=false;
 support3=createSprite(900,100,40,500);
 support3.visible=false;
 
@@ -113,14 +184,51 @@ char1.visible=false;
 char2.visible=false;
 text.visible=false;
 mhp.visible=true;
+life.visible=true;
+life1.visible=true;
+life2.visible=true;
+life3.visible=true;
+life4.visible=true;
+dementors1.visible=true;
+dementors2.visible=true;
+dementors3.visible=true;
+dementors4.visible=true;
+dementors5.visible=true;
+dementors6.visible=true;
+dementors7.visible=true;
+dementors8.visible=true;
+dementors9.visible=true;
+dementors10.visible=true;
+
   }
   if(mousePressedOver(char2)){
 
     char1.visible=false;
     char2.visible=false;
     text.visible=false;
+    life.visible=true;
+life1.visible=true;
+life2.visible=true;
+life3.visible=true;
+life4.visible=true;
     
+dementors1.visible=true;
+dementors2.visible=true;
+dementors3.visible=true;
+dementors4.visible=true;
+dementors5.visible=true;
+dementors6.visible=true;
+dementors7.visible=true;
+dementors8.visible=true;
+dementors9.visible=true;
+dementors10.visible=true;
+
+
+
       }
+
+
+
 
       if(keyDown("UP_ARROW")){
 
@@ -133,7 +241,8 @@ mhp.velocityY=-3;
         mhp.velocityY=3;
         
               }     
-    
+
+
 
   harrypotter.bounceOff(support1);
   harrypotter.bounceOff(support2);
@@ -144,6 +253,8 @@ mhp.velocityY=-3;
   
 
 drawSprites();
+
+
 }
 
 
@@ -152,7 +263,7 @@ drawSprites();
     var  dementors = createSprite(600,133);
      //  dementors.addImage(" dementors", dementorsImg);
 
- dementors.x=Math.round(random(130,135));     
+ 
     dementors.velocityX=-5;
      dementors.setLifetime=50;
      dementors.scale = 0.5;
